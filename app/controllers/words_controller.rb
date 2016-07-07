125988c96159a0c08462187a8ff685bdfd13cb65
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-07-06 23:47:12 -0400

    Add word create functionality

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,22 @@
 class WordsController < ApplicationController
   def index
-    @word = Word.order("RANDOM()").first
+    # this needs its own view for a random word
+    # @word = Word.order("RANDOM()").first
+    @words = Word.all
   end
 
   def new
     @word = Word.new
+  end
+
+  def create
+    Word.create(word_params)
+    redirect_to words_path
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:term)
   end
 end
